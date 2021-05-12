8525cce4df9820792f52d2e620d7996ed8acdd86
Robert <soriano@cbtseminary.org>
2021-05-12 12:02:02 -0500

    .

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ app.get('/*', (req, res) => {
     res.redirect(`https://cbtseminary.org/${req.originalUrl}`);
 });
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
