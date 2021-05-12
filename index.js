53dcad5ad67afafc94b0a1ae61a31fc1c96f6876
Robert <soriano@cbtseminary.org>
2021-05-12 11:50:52 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const app = require('express')();
+
+// The `res.redirect()` function sends back an HTTP 302 by default.
+// When an HTTP client receives a response with status 302, it will send
+// an HTTP request to the URL in the response, in this case `/to`
+app.get('/', (req, res) => {
+    console.log(req)
+    res.redirect('https://cbtseminary.org/blog');
+});
+
+
+app.get('/*', (req, res) => {
+    console.log(req.originalUrl)
+    res.redirect(`https://cbtseminary.org/${req.originalUrl}`);
+});
+
+const port = process.env.PORT || 80;
+app.listen(port, () => console.log(`Server started on port ${port}`));
